268bf277706359f3b8e625d6141139bd0a8fecb5
Kyle Weiss <Kyle@insidegood.com>
2013-05-18 22:28:23 -0600

    Let's actually randomize the first guess...

--- guessing_game.rb
+++ guessing_game.rb
@@ -6,26 +6,51 @@
 max = 100
 min = 1
 remaining_guesses = 6
+first_guess = rand(100) + 1
 
 while remaining_guesses > 1
 	remaining_guesses -= 1
-	guess = (min + max) / 2
-	puts "I will guess the number #{guess}."
-	puts "Did I get the right number?  Or should my next guess bigger or smaller?"
+	guess = first_guess
 
-	# If the guess is too big, the user should type smaller - or bigger, if it is too small
-	user_response = gets.chomp
+	if remaining_guesses == 5
+		puts "I will guess the number #{first_guess}."
 
-	# The computer uses the response to guess its next number
-	if user_response == "smaller"
-		puts "The number you are looking for is smaller than your guess."
-		max = guess
-	elsif user_response == "bigger"
-		puts "The number you are looking for is bigger than your guess."
-		min = guess
+		puts "Did I get the right number?  Or should my next guess bigger or smaller?"
+
+		# If the guess is too big, the user should type smaller - or bigger, if it is too small
+		user_response = gets.chomp
+
+		# The computer uses the response to guess its next number
+		if user_response == "smaller"
+			puts "The number you are looking for is smaller than your guess."
+			max = guess
+		elsif user_response == "bigger"
+			puts "The number you are looking for is bigger than your guess."
+			min = guess
+		else
+			puts "Whew, I got it on the first try!"
+			exit 0
+		end
+	
 	else
-		puts "Bwahahahahaa, I win!"
-		exit 0
+		guess = (min + max) / 2
+		puts "I will guess the number #{guess}."
+		puts "Did I get the right number?  Or should my next guess bigger or smaller?"
+
+		# If the guess is too big, the user should type smaller - or bigger, if it is too small
+		user_response = gets.chomp
+
+		# The computer uses the response to guess its next number
+		if user_response == "smaller"
+			puts "The number you are looking for is smaller than your guess."
+			max = guess
+		elsif user_response == "bigger"
+			puts "The number you are looking for is bigger than your guess."
+			min = guess
+		else
+			puts "Bwahahahahaa, I win!"
+			exit 0
+		end
 	end
 end
 
